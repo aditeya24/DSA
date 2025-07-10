5e70a9017d2bb982e99a5753dab2cf658138de86
aditeya24 <88970191+aditeya24@users.noreply.github.com>
2025-07-10 22:30:01 +0530

    Added insertion sort in 4_sort.c

--- Lab1/4_sort.c
+++ Lab1/4_sort.c
@@ -38,7 +38,18 @@ void selectionsort(int a[], int n)
 
 void insertionsort(int a[], int n)
 {
-
+  int key, j;
+  for(int i=1; i<n; i++)
+  {
+    key=a[i];
+    j=i-1;
+    while(j>=0 && a[j]<key)
+    {
+      a[j+1]=a[j];
+      j--;
+    }
+    a[j+1]=key;
+  }
 }
 
 void main()
