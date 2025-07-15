ea7d7f72002c63665089fef75a93322652ea756e
Aditeya J Frankur <88970191+aditeya24@users.noreply.github.com>
2025-07-15 14:51:52 +0530

    Create 1_stack.c

---
+++ Lab2/1_stack.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#define MAX 10
+
+int a[MAX];
+int top = -1;
+
+void push(int item)
+{
+	if(top == MAX-1)
+	{
+		printf("Stack overflow");
+	}
+	else
+	{
+		a[++top]=item;
+	}
+}
+
+int pop()
+{
+	int item;
+	if(top<0)
+	{
+		printf("Stack underflow");
+	}
+	else
+	{
+		item=a[top--];
+	}
+	return item;
+}
+
+int peek()
+{
+	if(top==-1)
+	{
+		printf("Stack empty");
+	}
+	return a[top];
+}
+
+void display()
+{
+	for(int i=0; i<=top; i++)
+	{
+		printf("%d ",a[i]);
+	}
+	printf("\n");
+}
+
+void main()
+{
+	int choice, item, removed, value;
+	while(1)
+	{
+		printf("\n1: Push\n2: Pop\n3: Peek\n4: Display\n0: Exit\n");
+		printf("Enter choice[1,2,3,0]: ");
+		scanf("%d",&choice);
+		if(choice==0)
+			break;
+		else if(choice==1)
+		{
+			printf("Enter value to push: ");
+			scanf("%d",&item);
+			push(item);
+			display();
+		}
+		else if(choice==2)
+                {
+                        removed = pop(item);
+			display();
+			printf("Removed: %d\n",removed);
+                }
+		else if(choice==3)
+                {
+                        value = peek(item);
+			printf("%d\n",value);
+                }
+		else if(choice==4)
+		{
+			display();
+		}
+    else
+    {
+      printf("ERROR: Invalid choice");
+    }
+	}
+}
