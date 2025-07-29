6804b7762a3f0d8a3a7c85ebd76ebe3b7a347ee7
aditeya24 <88970191+aditeya24@users.noreply.github.com>
2025-07-29 17:26:12 +0530

    Added display() in 1a_QueueOperations.c

--- Lab3/1a_QueueOperations.c
+++ Lab3/1a_QueueOperations.c
@@ -1,88 +1,89 @@
 #include <stdio.h> 
-#define MAX_SIZE 10
+#define MAX 10
 
 typedef struct {
-	int arr[MAX_SIZE];
+	int arr[MAX];
 	int rear;
 	int front;
 } Queue;
 
 int isFull(Queue *Q) {
-	if(Q->rear==MAX_SIZE-1)
-		return 1;
-	else
-		return 0;
+  return Q->rear == MAX - 1;
 }
 
 int isEmpty(Queue *Q) {
-	if(Q->front == -1 || Q->front > Q->rear)
-		return 1;
-	else
-		return 0;
+	return (Q->front == -1 || Q->front > Q->rear);
 }
 
-void ENQUEUE(Queue *Q,int item) {
-	if(isFull(Q))
-		printf("Queue overflow\n");
-	else
-	{
-		if(Q->front==-1 && Q->rear==-1)
-		{
+void enqueue(Queue *Q,int item) {
+	if (isFull(Q)) {
+    printf("Queue overflow\n");
+  } else {
+    if(Q->front==-1 && Q->rear==-1) {
 			Q->front=0;
 			Q->arr[++(Q->rear)]=item;
-		}
-		else
-			Q->arr[++(Q->rear)]=item;
+		} else {
+      Q->arr[++(Q->rear)]=item;
+    }
 	}
 }
 
-void DEQUEUE(Queue *Q) {
-	if(isEmpty(Q))
-		printf("Queue underflow\n");
-	else
-	{
-		int item=Q->arr[Q->front];
+void dequeue(Queue *Q) {
+	if (isEmpty(Q)) {
+    printf("Queue underflow\n");
+  }	else {
+		int item = Q->arr[Q->front];
 		printf("Dequeued: %d\n", item);
 
-		if(Q->front==Q->rear)
-		{
+		if (Q->front==Q->rear) {
 			Q->front=-1;
 			Q->rear=-1;
-		}
-		else
-		{
+		} else {
 			Q->front=Q->front+1;
-			
 		}
 	}
+}
+
+void display(Queue *Q) {
+  if(isEmpty(Q)) {
+    printf("Queue is Empty\n");
+    return;
+  }
+  printf("Queue: ");
+  int i = Q->front;
+  while (1) {
+    printf("%d ", Q->arr[i]);
+    if (i == Q->rear)
+      break;
+    i = (i + 1) % MAX;
+  }
+  printf("\n");
 }
 
 int main() {
-	Queue Q;
-	Q.front=-1;
-	Q.rear=-1;
+  Queue Q;
+  Q.front = -1;
+  Q.rear = -1;
 	int choice, item;
-	while(1)
-	{
-		printf("\n1: ENQUEUE\n2: DEQUEUE\n0: Exit\n");
+	while (1) {
+		printf("\n1: Enqueue\n2: Dequeue\n3: Display\n0: Exit\n");
 		printf("Enter choice[1,2,0]: ");
 		scanf("%d", &choice);
-		if(choice==0)
+		if (choice==0) {
 			break;
-		else if(choice==1)
-		{
-			printf("Enter value to ENQUEUE: ");
+		} else if (choice == 1) {
+			printf("Enter value to insert: ");
 			scanf("%d", &item);
-			ENQUEUE(&Q, item);
-		}
-		else if(choice==2)
-		{
-			DEQUEUE(&Q);
-		}
-		else
-		{
+			enqueue(&Q, item);
+			display(&Q);
+		} else if (choice == 2) {
+			dequeue(&Q);
+			display(&Q);
+		} else if (choice == 3) {
+			display(&Q);
+		} else {
 			printf("ERROR: Invalid choice\n");
-		}
+		}	
 	}
 	return 0;
 }
