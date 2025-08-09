ba0d2d921ad2b9498bfd7aaaf51c5c05e6ea618d
aditeya24 <88970191+aditeya24@users.noreply.github.com>
2025-08-09 11:55:49 +0530

    Fixed logic error

--- Lab2/1_stack.c
+++ Lab2/1_stack.c
@@ -4,99 +4,76 @@
 int a[MAX];
 int top = -1;
 
-int full()
-{
-  if(top == MAX-1)
-    return 1;
-  return 0;
+int full() {
+  return top == MAX-1;
 }
 
-int empty()
-{
-  if(top==0)
-    return 1;
-  return 0;
+int empty() {
+  return top == -1;
 }
 
-void display()
-{
-	for(int i=0; i<=top; i++)
-	{
-		printf("%d ",a[i]);
-	}
-	printf("\n");
+void display() {
+  if (empty()) {
+    printf("Stack is empty\n");
+    return;
+  }
+  for (int i = 0; i <= top; i++) {
+    printf("%d ",a[i]);
+  }
+  printf("\n");
 }
 
-void push(int item)
-{
-	if(full())
-	{
-		printf("Stack overflow\n");
-	}
-	else
-	{
-		a[++top]=item;
-		display();
-	}
+void push(int item) {
+  if (full()) {
+    printf("Stack overflow\n");
+    return;
+  }
+  a[++top]=item;
+  printf("Inserted: %d\n", item);
 }
 
-void pop()
-{
-	int item;
-	if(empty())
-	{
-		printf("Stack underflow\n");
-	}
-	else
-	{
-		printf("Removed: %d\n",a[top--]);
-		display();
-	}
+void pop() {
+  int item;
+  if (empty()) {
+    printf("Stack underflow\n");
+    return;	
+  }
+  item = a[top--];
+  printf("Removed: %d\n", item);
 }
 
-void peek()
-{
-	if(empty())
-	{
-		printf("Stack empty\n");
-	}
-	else
-	{
-		printf("%d\n", a[top]);
-	}
+void peek() {
+  if (empty()) {
+    printf("Stack is empty\n");
+    return;
+  }
+  printf("%d\n", a[top]);
 }
 
-void main()
-{
-	int choice, item, removed, value;
-	while(1)
-	{
-		printf("\n1: Push\n2: Pop\n3: Peek\n4: Display\n0: Exit\n");
-		printf("Enter choice[1,2,3,0]: ");
-		scanf("%d",&choice);
-		if(choice==0)
-			break;
-		else if(choice==1)
-		{
-			printf("Enter value to push: ");
-			scanf("%d",&item);
-			push(item);
-		}
-		else if(choice==2)
-                {
-                        pop(item);
-                }
-		else if(choice==3)
-                {
-                        peek(item);
-                }
-		else if(choice==4)
-		{
-			display();
-		}
-    else
-    {
+
+int main() {
+  int choice, item;
+  while(1) {
+    printf("\n1: Push\n2: Pop\n3: Peek\n4: Display\n0: Exit\n");
+    printf("Enter choice[1,2,3,4,0]: ");
+    scanf("%d", &choice);
+    if (choice==0) {
+      break;
+    } else if (choice==1) {
+      printf("Enter value to push: ");
+      scanf("%d", &item);
+      push(item);
+      display();
+    } else if (choice==2) {
+      pop();
+      display();
+    } else if (choice==3) {
+      peek();
+    } else if (choice==4) {
+      display();
+    } else {
       printf("ERROR: Invalid choice\n");
     }
-	}
+  }
+  return 0;
 }
