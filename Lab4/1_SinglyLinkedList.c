5d5433e617c57de2d2e7610a1dd1142196c36475
aditeya24 <88970191+aditeya24@users.noreply.github.com>
2025-09-12 13:57:38 +0530

    Created 1_SinglyLinkedList.c

---
+++ Lab4/1_SinglyLinkedList.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+typedef struct {
+  int data;
+  struct Node *next;
+} Node;
+
+void insertFront (Node *n,) {
+
+}
+
+void insertRear (Node *n) {
+
+}
+
+void insertAtPosition (Node *n, int data, int item) {
+
+}
