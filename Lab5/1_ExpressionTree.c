eed9dc9294ce8cfa3b80f90af11d61dd5fbd5f5e
Aditeya J Frankur <88970191+aditeya24@users.noreply.github.com>
2025-10-07 16:34:06 +0530

    Update 1_ExpressionTree.c

--- Lab5/1_ExpressionTree.c
+++ Lab5/1_ExpressionTree.c
@@ -8,106 +8,129 @@ typedef struct Node {
 	struct Node* right;
 } Node;
 
+Node* headTree = NULL;
 
-int j = 0;
+typedef struct Stack {
+	struct Node* ptr;
+	struct Stack* next;
+} Stack;
+
+Stack *headA = NULL;
+Stack *headB = NULL;
+
 char infix[MAX];
-char postfix[MAX];
-char a[MAX];
-int top = -1;
 
-int full() {
-  return top == MAX-1;
-}
-
-int empty() {
-  return top == -1;
-}
-
-void display() {
-  if (empty()) {
-    printf("Stack is empty\n");
-    return;
-  }
-  for (int i = 0; i <= top; i++) {
-    printf("%d ",a[i]);
-  }
-  printf("\n");
-}
-
-void push(char item) {
-  if (full()) {
-    printf("Stack overflow\n");
-    return;
-  }
-  a[++top]=item;
-}
-
-char pop() {
-  char item;
-  if (!empty()) {
-    item = a[top--];
-    return item;
-  }
-}
-
-char peek() {
-  if (!empty()) {
-    return a[top];
-  }
+Node* createNode(int data) { 
+        Node* newNode = (Node*)malloc(sizeof(Node)); 
+        newNode->data = data; 
+        newNode->left = NULL; 
+        newNode->right = NULL; 
+        return newNode; 
 }
 
 int precedence(char c) {
-  switch (c) {
-    case '+':
-      return 1;
-    case '-':
-      return 1;
-    case '*':
-      return 2;
-    case '/':
-      return 2;
-    case '^':
-      return 3;
-  }
-  return 0;
+	switch (c) {
+		case '+':
+			return 1;
+		case '-':
+			return 1;
+		case '*':
+			return 2;
+		case '/':
+			return 2;
+		case '^':
+			return 3;
+		}
+	return 0;
 }
 
-void infixToPostfix(char s[]) {
-  top = -1;
-  int i = 0, j = 0;
-  char item;
-  push('#');
-  while (s[i] != '\0') {
-    item = s[i];
-    if ( (item >= 'a' && item <= 'z') || (item >= 'A' && item <= 'Z') || (item >= '0' && item <= '9') ) {
-      postfix[j++] = s[i];
-    } else if (item == ')') {
-      while (peek() != '(') {
-        postfix[j++] = pop();
-      }
-      pop();
-    } else if (item == '(') {
-      push(item);
-    } else {
-      while (precedence(peek()) >= precedence(item) || (precedence(peek()) == precedence(item) && item != '^')) {
-        postfix[j++] = pop();
-      }
-      push(item);
-    }
-    i++;
-  }
-
-  while (peek() != '#') {
-    postfix[j++] = pop();
-  }
-  postfix[j] = '\0';
-
-  printf("Postfix: %s\n", postfix);
+void push(int data) {
+	Node *newNode = createNode(data);
+	if (newNode == NULL) {
+		printf("ERROR: Memory insufficient\n");
+		return;
+	}
+	newNode->next = head;
+	head = newNode;
 }
+
+Node* pop() {
+	Node *toDelete;
+	if (head == NULL) {
+		printf("Stack is already empty\n");
+		return;
+	}
+	toDelete = head;
+	head = head->next;
+	printf("Popped: %d\n", toDelete->data);
+	return toDelete;
+	free(toDelete);
+}
+
+char peek() {
+	if (head == NULL) {
+		printf("Stack is empty\n");
+		return;
+	}
+	return headS->ptr->data;
+}
+
+void createExpressionTree (char *str) {
+	top = -1;
+	int i = 0, j = 0;
+	char item;
+	while (curr != NULL) {
+		item = curr->data	Node *toDelete;
+	if (head == NULL) {
+		printf("Stack is already empty\n");
+		return;
+	}
+	toDelete = head;
+	head = head->next;
+	printf("Popped: %d\n", toDelete->data);
+	free(toDelete);;
+		if ( (item >= 'a' && item <= 'z') || (item >= 'A' && item <= 'Z') || (item >= '0' && item <= '9') ) {
+			push(
+}
+
+void createExpressionTree(char s[]) {
+	top = -1;
+	int i = 0, j = 0;
+	char item;
+	while (s[i] != '\0') {
+		item = s[i];
+		if ( (item >= 'a' && item <= 'z') || (item >= 'A' && item <= 'Z') || (item >= '0' && item <= '9') ) {
+			push(s[i], headA);
+		} else if (item == ')') {
+			while (peek() != '(') {
+				postfix[j++] = pop();
+			}
+			pop();
+		} else if (item == '(') {
+			push(item);
+		} else {
+			while (precedence(peek()) >= precedence(item) || (precedence(peek()) == precedence(item) && item != '^')) {
+				postfix[j++] = pop();
+			}
+			push(item);
+		}
+		i++;
+	}
+
+	while (peek() != '#') {
+		postfix[j++] = pop();
+	}
+	postfix[j] = '\0';
+	
+	return postfix;
+}
+
+ 
 
 int main() {
 	printf("Enter Infix expression: ");
 	scanf("%s", infix);
-	infixToPostfix(infix);
+	const char* postfix = infixToPostfix(infix);
+	
 	return 0;	
 }
